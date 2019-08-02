4cb6b630eaa30b982f48c8479abf6a53dfeb415b
MuradUsa <muradusa@Murad.local>
2019-08-01 22:22:05 -0700

    timer is set

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,36 @@
-console.log("hi");
+
+// var game = function(){
+//     alert ("Game has Started");
+// }
+
+var timer = 30;
+var intervalId;
+
+
+
+
+$(document).ready(function(){
+    $(".start").on("click",function(){
+        $(".container").html(game());
+        
+        
+        
+        
+    })
+})
+
+function game(){
+
+    intervalId = setInterval(decrement, 1000);
+
+function decrement(){
+    timer--;
+    $("#show-number").html(timer);
+    if (timer === 0){
+        stop();
+        alert ("time up!");
+    }
+  
+    
+}
+}
